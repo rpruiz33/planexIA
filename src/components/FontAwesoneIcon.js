93fa4d63b00106538a0c6e96e0e6c67bf87a6960
unknown <rpruiz33@gmail.com>
2024-02-01 16:05:02 -0300

    seguimos con los estilos

--- src/components/FontAwesoneIcon.js
+++ src/components/FontAwesoneIcon.js
@@ -1,23 +1,15 @@
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faLinkedin, faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
 import './FontAwesoneIcon.css';
 
 const FontAwesoneIcon = () => {
   return (
     <>
-    
-     <FontAwesomeIcon icon={faLinkedin}size="2x" style={{ color: 'black' ,     margin: '0px -101px 0px -99px'}}/>
-    
-   
-      <FontAwesomeIcon icon={faFacebook} size="2x" style={{ color: 'black',         margin: '6px -50px 5px -43px'}}/>
-    
-      
-   
-    <FontAwesomeIcon icon={faSquareInstagram} size="2x" style={{ color: 'black', margin: '0px -10px 0px 0px}'}}/>
-      
-  
+      <FontAwesomeIcon icon={faLinkedin} size="2x" className="icon icon-linkedin" />
+
+      <FontAwesomeIcon icon={faFacebook} size="2x" className="icon icon-facebook" />
+
+      <FontAwesomeIcon icon={faSquareInstagram} size="2x" className="icon icon-instagram" />
     </>
   );
 };
