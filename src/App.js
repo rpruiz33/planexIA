1b1f6a76830cd8bc328db56fad1c8ae5101b7b62
unknown <rpruiz33@gmail.com>
2024-02-09 18:09:25 -0300

    probando responsive navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 
+
 import ContactForm from './components/ContactForm';
 import ServiceDetail from './components/ServiceDetail';
 import Product from './components/Product';
