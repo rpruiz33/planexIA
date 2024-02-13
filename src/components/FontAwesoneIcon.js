fd92dbcbf6098bd51b33656af4be4bcbe45df990
unknown <rpruiz33@gmail.com>
2024-02-13 20:29:14 -0300

    estilos navbar

--- src/components/FontAwesoneIcon.js
+++ src/components/FontAwesoneIcon.js
@@ -6,7 +6,7 @@ const FontAwesoneIcon = () => {
   return (
     <>
    <a href="https://www.linkedin.com/in/planexia-s-a-3571b52b4/" target="_blank" className="icon3">
-        <FontAwesomeIcon icon={faLinkedin} size="2x" className="icon icon-linkedin" />
+        <FontAwesomeIcon icon={faLinkedin} size="2x" className="icon icon-instagram" />
       </a>
 
 
