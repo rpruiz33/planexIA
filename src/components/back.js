91a61d027241137b94088c28e929a796d866c67e
unknown <rpruiz33@gmail.com>
2024-02-14 19:12:18 -0300

    service list arreglando péro falta

--- src/components/back.js
+++ src/components/back.js
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBackward } from '@fortawesome/free-solid-svg-icons';
+import { faLeftLong} from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 const FontAwesoneIcon = () => {
   return (
     <>
-      <Link to={`/`} className="service-link">
-        <FontAwesomeIcon icon={faBackward} size="2x" color='black'/>
+      <Link to={`/`} className="service-link2">
+      <FontAwesomeIcon icon={faLeftLong} size="3x" color='black'/>
       </Link>
     </>
   );
