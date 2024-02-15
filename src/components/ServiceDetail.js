868a33ce893041ad9b271d73385541d89cb0954b
unknown <rpruiz33@gmail.com>
2024-02-14 23:16:31 -0300

    servicios1212

--- src/components/ServiceDetail.js
+++ src/components/ServiceDetail.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import './ServiceDetail.css';
 import { NavLink } from 'react-router-dom';
 import Back from './back';
+
 const ServiceDetail = () => {
   const { id } = useParams();
 
@@ -10,42 +11,43 @@ const ServiceDetail = () => {
     1: {
       name: 'Servicio Empresarial',
       description: 'Asesoramiento en finanzas y gestión económica.',
-      additionalDetails: 'Ofrecemos soluciones personalizadas para el crecimiento de tu empresa.',
+      additionalDetails: 'Ofrecemos soluciones personalizadas para el crecimiento de tu empresa.'
     },
     2: {
       name: 'Servicio Financiero',
       description: 'Asesoramiento y servicios financieros para empresas y particulares.',
-      additionalDetails: 'Gestión de inversiones, planificación financiera y más.',
+      additionalDetails: 'Gestión de inversiones, planificación financiera y más.'
     },
     3: {
       name: 'Servicio Agropecuario',
       description: 'Asesoramiento integral para el sector agropecuario.',
-      additionalDetails: 'Optimización de procesos, gestión de recursos y más.',
+      additionalDetails: 'Optimización de procesos, gestión de recursos y más.'
     },
     4: {
       name: 'Servicio Impexpo',
       description: 'Asesoramiento en comercio internacional y exportaciones.',
-      additionalDetails: 'Facilitamos el proceso de importación y exportación de productos.',
+      additionalDetails: 'Facilitamos el proceso de importación y exportación de productos.'
     },
     5: {
       name: 'ERP && CRM',
       description: 'Soluciones integradas de planificación de recursos empresariales y gestión de relaciones con clientes.',
-      additionalDetails: 'Mejora la eficiencia operativa y la interacción con tus clientes.',
+      additionalDetails: 'Mejora la eficiencia operativa y la interacción con tus clientes.'
     },
     6: {
       name: 'Seguimiento e Indicadores',
       description: 'Desarrollo de sistemas de seguimiento y medición de indicadores clave.',
-      additionalDetails: 'Obtén información valiosa para la toma de decisiones estratégicas.',
+      additionalDetails: 'Obtén información valiosa para la toma de decisiones estratégicas.'
     },
   };
 
   const service = serviceDetails[id];
+  const serviceClassName = `service-${id}`;
 
   return (
-    <div className="service-detail">
-       <NavLink to="/">
+    <div className={`service-detail ${serviceClassName}`}>
+      <NavLink to="/">
         <Back/>
-        </NavLink>
+      </NavLink>
       <h2>{service.name}</h2>
       <p>{service.description}</p>
       <p>{service.additionalDetails}</p>
