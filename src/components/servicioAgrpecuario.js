762ee57d8c3ece5a439ef66159939063b744983d
unknown <rpruiz33@gmail.com>
2024-03-08 19:17:19 -0300

    styles33agro

--- src/components/servicioAgrpecuario.js
+++ src/components/servicioAgrpecuario.js
@@ -5,11 +5,13 @@ const Service3 = () => {
   return (
     <div className='service-detail'>
       <Back/>
-      
+      <div className="contenedor">
       <h2>Servicio Agropecuario</h2>
-      <p>Asesoramiento integral para el sector agropecuario.</p>
-      <p>Optimización de procesos, gestión de recursos y más.</p>
+      <p>#Asesoramiento integral para el sector agropecuario.</p>
+      <p>#Optimización de procesos, gestión de recursos y más.</p>
     </div>
+    </div>
+     
   );
 };
 
