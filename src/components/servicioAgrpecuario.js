34a2ebc0fa006a18af72744cab47843c9bb1a5d7
unknown <rpruiz33@gmail.com>
2024-03-11 16:41:41 -0300

    terminado agro 1440

--- src/components/servicioAgrpecuario.js
+++ src/components/servicioAgrpecuario.js
@@ -7,8 +7,8 @@ const Service3 = () => {
       <Back/>
       <div className="contenedor">
       <h2>Servicio Agropecuario</h2>
-      <p>#Asesoramiento integral para el sector agropecuario.</p>
-      <p>#Optimización de procesos, gestión de recursos y más.</p>
+      <p className='p3'>#Asesoramiento integral para el sector agropecuario.</p>
+      <p className='p3'>#Optimización de procesos, gestión de recursos y más.</p>
     </div>
     </div>
      
