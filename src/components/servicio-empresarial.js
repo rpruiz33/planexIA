0b32524fff9790645a8ed096e38e3bbeaa151d20
unknown <rpruiz33@gmail.com>
2024-03-15 12:16:22 -0300

    1024 1366 p

--- src/components/servicio-empresarial.js
+++ src/components/servicio-empresarial.js
@@ -6,7 +6,7 @@ const Service1 = () => {
     <div className='service-detail'>
          <Back/>
          <div className="contenedor1">
-      <h2  >Servicio Empresarial</h2>
+      <h2 className='empre3' >Servicio Empresarial</h2>
       <p className='empre'>#Asesoramiento en finanzas y gestión económica.</p>
 
       <p className='empre1' >#Ofrecemos soluciones personalizadas para el crecimiento de tu empresa.</p>
