112fc23fa048176850eddf4917a446108588e5c1
unknown <rpruiz33@gmail.com>
2024-03-26 17:00:10 -0300

    un poco de back con el contacto

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,69 +1,58 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import './ContactForm.css';
 import SuccessMessage from './SuccessMessage';
+
 const ContactForm = () => {
+  const form = useRef();
   const [isSuccess, setIsSuccess] = useState(false);
 
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    const formData = new URLSearchParams();
-    formData.append('name', e.target.name.value);
-    formData.append('email', e.target.email.value);
-    formData.append('message', e.target.message.value);
 
-    try {
-      const response = await fetch('http://localhost:5000/submit-form', {
-        method: 'POST',
-        body: formData
-      });
-      
-      // Verificar si la respuesta fue exitosa
-      if (response.ok) {
-        setIsSuccess(false); // Establecer el estado de éxito a true
-      } else {
-        setIsSuccess(true); // Si hay algún error en la respuesta, establecer el estado de éxito a false
-      }
-    } catch (error) {
-      console.error('Error al enviar los datos:', error);
-      setIsSuccess(true); // Si hay un error al enviar los datos, establecer el estado de éxito a false
+    emailjs
+      .sendForm('service_y99ax2q', 'template_661dwxp', form.current, {
+        publicKey: 'FoFhSWkjyUnaPyFKW',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          setIsSuccess(true); // Establece el estado de éxito en verdadero después de enviar el formulario
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+  };
+
+  // Renderiza el formulario solo si isSuccess es falso
+  const renderForm = () => {
+    if (!isSuccess) {
+      return (
+        <form ref={form} onSubmit={sendEmail}>
+          <h2 className='con'>Contacto</h2>
+          <div className="form-group">
+            <label htmlFor="name">Nombre:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Correo electrónico:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group1">
+            <label htmlFor="message">Mensaje:</label>
+            <textarea className='box' id="message" name="message" required />
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
+      );
     }
   };
 
-  let content;
-  if (!isSuccess) {
-    content = (
-      <form onSubmit={handleSubmit} method="POST" action="http://localhost:5000/submit-form">
-          <h2 className='con'>Contacto</h2>
-        <div className="form-group">
-          <label htmlFor="name">Nombre:</label>
-          <input type="text" id="name" name="name" required />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="email">Correo electrónico:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-
-        <div className="form-group1">
-          <label htmlFor="message">Mensaje:</label>
-          <textarea className='box' id="message" name="message" required />
-        </div>
-
-        <button type="submit">Enviar</button>
-      </form>
-    );
-  } else {
-    content = (
-      <div>
-        <SuccessMessage />
-      </div>
-    );
-  }
-
   return (
-    <div className="contact-form-container" style={{ fontFamily: 'Oswald, sans-serif' }}>
-    
-      {content}
+    <div className="contact-form-container">
+      {/* Renderiza el formulario o el componente SuccessMessage dependiendo del estado de isSuccess */}
+      {isSuccess ? <SuccessMessage /> : renderForm()}
     </div>
   );
 };
